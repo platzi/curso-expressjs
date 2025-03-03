69d678a7cb22d672d77d588a57c76105c76a66da
Oscar Barajas Tavares <gndx@MacBook-Pro-de-Oscar.local>
2025-03-03 16:46:05 -0500

    [ADD] bodyParser formData/JSON

--- app.js
+++ app.js
@@ -33,6 +33,31 @@ app.get('/search', (req, res) => {
     `);
 });
 
+app.post('/form', (req, res) => {
+  const name = req.body.nombre || 'Anónimo';
+  const email = req.body.email || 'No proporcionado';
+  res.json({
+    message: 'Datos recibidos',
+    data: {
+      name,
+      email
+    }
+  });
+});
+
+app.post('/api/data', (req, res) => {
+  const data = req.body;
+
+  if (!data || Object.keys(data).length === 0) {
+    return res.status(400).json({ error: 'No se recibieron datos' });
+  }
+
+  res.status(201).json({
+    message: 'Datos JSON recibidos',
+    data
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor: http://localhost:${PORT}`);
 });
