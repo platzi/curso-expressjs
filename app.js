9e17d78e967829c368eec087a477f0a747648dba
Oscar Barajas Tavares <gndx@MacBook-Pro-de-Oscar.local>
2025-03-03 16:35:12 -0500

    [ADD] dynamic routes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
@@ -10,6 +14,22 @@ app.get('/', (req, res) => {
       <h1>Curso Express.js V3</h1>
       <p>Esto es una aplicación node.js con express.js</p>
       <p>Corre en el puerto: ${PORT}</p>
+    `);
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Mostrar información del usuario con ID: ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+  const terms = req.query.termino || 'No especificado';
+  const category = req.query.categoria || 'Todas';
+
+  res.send(`
+      <h2>Resultados de Busqueda:</h2>
+      <p>Término: ${terms}</p>
+      <p>Categoría: ${category}</p>
     `);
 });
 
