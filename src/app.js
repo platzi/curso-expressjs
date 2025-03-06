1349323b1b38082502afd44d37fde083ade73523
Oscar Barajas Tavares <gndx@MacBook-Pro-de-Oscar.local>
2025-03-06 18:13:31 -0500

    [ADD] app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 
+const LoggerMiddleware = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+
 app.use(express.json());
+app.use(LoggerMiddleware);
+app.use(errorHandler);
 
 app.use('/api', routes);
 
